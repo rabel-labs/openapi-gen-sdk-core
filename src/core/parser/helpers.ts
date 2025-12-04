2dfb58115795de956b72d5aeb231802c8c1fd50c
Rabel <46533182+Montreal-Dev@users.noreply.github.com>
2025-12-03 21:39:54 -0500

    Refactor parser handler to use plugins and refractor
    Updated createParserHandler to accept plugins and a refractor, simplifying handler logic and improving extensibility. Refactored operationId command to use the new plugin-based approach for normalization, removing direct refract calls from handlers.

--- src/core/parser/helpers.ts
+++ src/core/parser/helpers.ts
@@ -3,24 +3,52 @@ import { ResolvedConfig } from '@/core/parser/operationId/action';
 import { PredicateFunc } from '@/core/predicate';
 
 import { Element } from '@swagger-api/apidom-core';
+import { OpenApi3_1Element } from '@swagger-api/apidom-ns-openapi-3-1';
+
+type Refractable = {
+  refract: typeof Element.refract;
+};
+function isRefractable(value: any): value is Refractable {
+  return typeof value?.refract === 'function';
+}
+type RefractProcess = () => void;
+type RefractVisitor<E extends Element> = {
+  [key in E['element']]?: {
+    enter?: (...args: any[]) => void;
+    leave?: (...args: any[]) => void;
+  };
+};
+
+type RefractablePlugin = (toolbox?: any) => {
+  name?: string;
+  pre?: RefractProcess;
+  visitor: RefractVisitor<Element>;
+  post?: RefractProcess;
+};
 
 /**
  * Create a VisitorHandler.
- * @param predicate - ElementPredicate<E>
- * @param handler - (element: E) => T
+ * @param predicate - PredicateFunc<E>
+ * @param handler - (element: E, options?: Partial<ResolvedConfig>) => T
+ * @param refractor - Refractable - default Element
  * @returns - VisitorHandler<E, T>
  */
 export function createParserHandler<E extends Element>(
   name: ParserCommandName,
   predicate: PredicateFunc<E>,
-  handler: (element: E, options?: Partial<ResolvedConfig>) => E,
+  plugins: (options?: Partial<ResolvedConfig>) => RefractablePlugin[],
+  refractor: Refractable = Element,
 ): CommandParserHandler<E> {
   return {
     name,
     predicate,
     handler: (element: E, options?: Partial<ResolvedConfig>) => {
-      // element is Element, but predicate ensures it's E
-      return handler(element as E, options);
+      const refractorTarget = refractor ?? element.constructor;
+      if (!isRefractable(refractorTarget)) throw new Error('ParserCommander: no refractor found');
+      const h = OpenApi3_1Element.refract(element, {
+        plugins: plugins(options) ?? [],
+      }) as E;
+      return h;
     },
   };
 }
--- src/core/parser/operationId/command.ts
+++ src/core/parser/operationId/command.ts
@@ -3,34 +3,26 @@ import { createParserHandler } from '@/core/parser/helpers';
 import { createOperationIdParser } from '@/core/parser/operationId/action';
 import { isOpenApi2, isOpenApi3x } from '@/core/predicate';
 
-import { SwaggerElement } from '@swagger-api/apidom-ns-openapi-2';
-import { OpenApi3_0Element } from '@swagger-api/apidom-ns-openapi-3-0';
 import {
   OpenApi3_1Element,
   refractorPluginNormalizeOperationIds,
 } from '@swagger-api/apidom-ns-openapi-3-1';
 
 const operationIdParsers: CommandParserHandler[] = [
-  createParserHandler('operationId', isOpenApi2, (element, options) => {
-    const openApiElement = SwaggerElement.refract(element, {
-      plugins: [
-        refractorPluginNormalizeOperationIds({
-          operationIdNormalizer: createOperationIdParser(options),
-        }),
-      ],
-    }) as SwaggerElement;
-    return openApiElement;
-  }),
-  createParserHandler('operationId', isOpenApi3x, (element, options) => {
-    const openApiElement = OpenApi3_1Element.refract(element, {
-      plugins: [
-        refractorPluginNormalizeOperationIds({
-          operationIdNormalizer: createOperationIdParser(options),
-        }),
-      ],
-    }) as OpenApi3_1Element | OpenApi3_0Element;
-    return openApiElement;
-  }),
+  createParserHandler('operationId', isOpenApi2, (options) => [
+    refractorPluginNormalizeOperationIds({
+      operationIdNormalizer: createOperationIdParser(options),
+    }),
+  ]),
+  createParserHandler(
+    'operationId',
+    isOpenApi3x,
+    (options) => [
+      refractorPluginNormalizeOperationIds({
+        operationIdNormalizer: createOperationIdParser(options),
+      }),
+    ],
+    OpenApi3_1Element,
+  ),
 ];
-
 export default operationIdParsers;
